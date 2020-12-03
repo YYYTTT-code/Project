8dcfe33bbf86c4e9466e7826fa95ce2e62429dab
YYYTTT-code <yt1172910316@163.com>
2020-12-03 21:42:40 +0800

    调整格式

--- src/com/hit/yt/Main.java
+++ src/com/hit/yt/Main.java
@@ -1,8 +1,15 @@
 package com.hit.yt;
 
+import java.util.*;
+import java.io.*;
+
 public class Main {
     static int[][] square;
+    public static Scanner in;
 
+    public static boolean isLeaglMagicSquare(String fileName){
+        return true;
+    }
 
     public static void main(String[] args) {
     }
